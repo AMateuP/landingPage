ff45193d9f666c2bc69cccae7bfa4967089b6569
toni <amateup@esliceu.net>
2024-05-28 13:58:00 +0200

    Nueva rama

--- index.php
+++ index.php
@@ -1,50 +1,6 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mi proyecto web</title>
-    <link rel="stylesheet" href="CSS/normalize.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Jersey+15&family=Roboto:wght@400;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="CSS/styles.css">
-</head>
-<body>
-    <header>
-        <h1 class="titulo">La web de <span>Cubo Juegos</span></h1>
-    </header>
-    <div class="bg-secundario">
-        <nav class="navegacion-principal">
-            <a href="Tutoriales.html">Tutoriales</a>
-            <a href="Novedades.html">Novedades</a>
-            <a href="Contacto.html">Contacto</a>
-        </nav>
-    </div>
-    <section class="hero">
-        <div class="contenido-hero">
-            <h2>Formacion a tu ritmo</h2>
-            <div class="informacion">
-                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-3d-cube-sphere" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ff2825" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                    <path d="M6 17.6l-2 -1.1v-2.5" />
-                    <path d="M4 10v-2.5l2 -1.1" />
-                    <path d="M10 4.1l2 -1.1l2 1.1" />
-                    <path d="M18 6.4l2 1.1v2.5" />
-                    <path d="M20 14v2.5l-2 1.12" />
-                    <path d="M14 19.9l-2 1.1l-2 -1.1" />
-                    <path d="M12 12l2 -1.1" />
-                    <path d="M18 8.6l2 -1.1" />
-                    <path d="M12 12l0 2.5" />
-                    <path d="M12 18.5l0 2.5" />
-                    <path d="M12 12l-2 -1.12" />
-                    <path d="M6 8.6l-2 -1.1" />
-                </svg>
-                <p>Aprende desde casa</p>
-            </div>
-            <a class="boton" href="Guías_y_Tutoriales.html">Tutoriales</a>
-        </div>
-    </section>
+<?php
+    include 'header.php';
+?>
     <main class="contenedor sombra">
         <h2>Mi contenido</h2>
         <div class="micontenido">
@@ -128,8 +84,6 @@
             </form>
         </section>
     </main>
-    <footer>
-        <p>Todos los derechos reservados</p>
-    </footer>
-</body>
-</html>
+<?php
+    include 'footer.php';
+?>
