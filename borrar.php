6fb5ff4e01cd88c94c82ce2b3d2e3dc35d15a4f8
toni <amateup@esliceu.net>
2024-06-13 19:10:26 +0200

    Finish him

---
+++ borrar.php
@@ -0,0 +1,13 @@
+<?php
+include 'db.php';
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+    $id = $_GET['id'];
+    $sql = "DELETE FROM contactos WHERE id=?";
+    $stmt = $conn->prepare($sql);
+
+    // Vincular el parámetro y ejecutar la declarcion
+    $stmt->bind_param("i", $id);
+    $result = $stmt->execute();
+    $msg="Registro borrado con exito";
+    header("Location: read.php?status=success&msg=$msg");
+}
